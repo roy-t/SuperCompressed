eba7fe45c5ce8cf38fd3c84c695e9f4275e1ec8a
Roy Triesscheijn <roy-t@hotmail.com>
2022-05-16 20:44:46 +0200

    Include fmt library

--- BasisCLR/Encoder.cpp
+++ BasisCLR/Encoder.cpp
@@ -1,13 +1,25 @@
 #include "Encoder.h"
-#include <basisu.h>
-////#include <basisu_transcoder_internal.h>
-#include <basisu_enc.h>
 
-#pragma managed(push, off)
+#include <basisu.h>
+#include <basisu_enc.h>
+#include <fmt/format.h>
 
 void Encoder::Init()
 {
 	basisu::basisu_encoder_init();
 }
 
-#pragma managed(pop)
+void Encoder::Encode(const std::string &filename)
+{
+	basisu::image image;
+	if (!basisu::load_image(filename, image))
+	{
+		std::string error = fmt::format("Failed to load image {}.", filename);		
+		throw std::exception(error.c_str());
+	}
+}
+
+void Encoder::Deinit()
+{
+	basisu::basisu_encoder_deinit();
+}
--- BasisCLR/Encoder.h
+++ BasisCLR/Encoder.h
@@ -1,9 +1,14 @@
 #pragma once
 
+#include <string>
 
 class Encoder
 {
 public:
 	void Init();
+
+	void Encode(const std::string& filename);
+	
+	void Deinit();
 
 };
