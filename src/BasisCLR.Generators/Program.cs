fed1f22c50c8f65415081aa39046eb70090a2335
Roy Triesscheijn <roy-t@hotmail.com>
2022-06-04 21:51:03 +0200

    WIP generating

---
+++ src/BasisCLR.Generators/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+//// TODO
+
+//// read D:\Projects\C#\SuperCompressedDotNet\src\external\basis_universal\encoder\basisu_comp.h 
+//// lines 319 to 443
+
+//// See https://aka.ms/new-console-template for more information
+
+using BasisCLR.Generators;
+using CommandLine;
+
+Parser.Default.ParseArguments<ExtractOptions, GenerateOptions>(args)
+    .WithParsed<ExtractOptions>(e =>
+    {
+        Console.WriteLine($"Extracting {e.Input}:[{e.From}..{e.To}] to {e.Output}");
+        HeaderParser.Parse(e.Input, e.From, e.To);
+
+    })
+    .WithParsed<GenerateOptions>(g =>
+    {
+        YamlParser.Parse(g.Input);
+    });
+
+//// Yay raw string literals fancy pancy
+//var hello = "";
+//var v = 
+//    $$""""
+//    Hello World
+//    Yes baby
+//    {rhhreher}
+//    """{{hello}}
+//        rearlerler'r'q439501"5353"
+//    """";
+
+
+//Console.WriteLine(v);
+//Console.WriteLine("Now what");
---
+++ src/BasisCLR.Generators/YamlParser.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace BasisCLR.Generators
+{
+    internal class ParameterList
+    {
+        public List<Parameter> Parameters { get; set; } = new List<Parameter>();
+    }
+
+    internal class Parameter
+    {        
+        public string Name { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty;
+        public string Default { get; set; } = string.Empty;
+        public string Comment { get; set; } = string.Empty;
+
+        public override string ToString()
+        {
+            var output = $"{this.Type} {this.Name}";
+            if(!string.IsNullOrEmpty(this.Default))
+            {
+                output += $"  = {this.Default}";
+            }
+
+            if (!string.IsNullOrEmpty(this.Comment))
+            {
+                output += $" // {this.Comment}";
+            }
+            return output;
+        }
+    }
+
+    internal class YamlParser
+    {
+        public static ParameterList Parse(string filename)
+        {
+            var deserializer = new DeserializerBuilder()
+                .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                .Build();
+
+            var input = File.ReadAllText(filename);
+            var parameters = deserializer.Deserialize<ParameterList>(input);
+
+            return parameters;
+        }
+    }
+}
