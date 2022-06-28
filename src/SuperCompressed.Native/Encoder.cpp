870aaaefd9642272f22ecc8d3bd8a986893e80a5
Roy Triesscheijn <roy-t@hotmail.com>
2022-06-28 20:14:31 +0200

    WIP

---
+++ src/SuperCompressed.Native/Encoder.cpp
@@ -0,0 +1,12 @@
+#include "pch.h"
+#include "Encoder.h"
+
+#include <basisu.h>
+
+void Encoder::Ten()
+{
+}
+
+void Fourteen()
+{
+}
---
+++ src/SuperCompressed.Native/Encoder.h
@@ -0,0 +1,11 @@
+#pragma once
+//#include <string>
+
+void Fourteen();
+
+class Encoder
+{
+public:
+	void Ten();
+};
+
