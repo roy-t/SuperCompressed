7885d16651d00b183d7ac9ec635c94141ba3a359
Roy Triesscheijn <roy-t@hotmail.com>
2022-07-06 17:11:37 +0200

    Working Nuget package, but messy project directories

--- src/SuperCompressed.Tests/UnitTests.cs
+++ src/SuperCompressed.Tests/UnitTests.cs
@@ -40,18 +40,18 @@ namespace SuperCompressed.Tests
 
             var formats = Enum.GetValues<TranscodeFormats>();
 
-            foreach(var targetFormat in formats)
+            foreach (var targetFormat in formats)
             {
                 foreach (var imageIndex in Enumerable.Range(0, images))
                 {
                     foreach (var levelIndex in Enumerable.Range(0, levels))
                     {
                         using var transcoded = transcoder.Transcode(encoded.Data, imageIndex, levelIndex, targetFormat);
-                        True(transcoded.Data.Length > 0);                        
-                        Equal(128 >> levelIndex , transcoded.Width);
+                        True(transcoded.Data.Length > 0);
+                        Equal(128 >> levelIndex, transcoded.Width);
                         Equal(128 >> levelIndex, transcoded.Heigth);
                         Equal(targetFormat, transcoded.Format);
-                        
+
                         // For compressed formats the pitch is only > 0 if the the size of the image is larger than
                         // the block size, for uncompressed formats the block size is 1x1 so there's no such exception
                         if (transcoded.Width >= 4)
@@ -60,7 +60,7 @@ namespace SuperCompressed.Tests
                         }
                     }
                 }
-            }                        
+            }
         }
 
         public static class TestUtilities
